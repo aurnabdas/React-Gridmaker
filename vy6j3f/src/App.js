051f73720e847082d1c7a72e4f0f5ce7944b770c
aurnabdas <aurnabdas@Aurnabs-MBP.lan>
2023-10-29 19:03:10 -0400

    made the addRow, removeRow, addColumn and removeColumn more readable with comments

--- vy6j3f/src/App.js
+++ vy6j3f/src/App.js
@@ -10,28 +10,42 @@ function Grid() {
     setSelectedColor(color);
   };
 
+  // you use a copy of the grid, so it doesnt affect the rendering
+  // when using setGrid you want to return a 2d array: [[],[]]. that is why everything is within the []
+  //...grid will return the array inside: ['white], you are combining this with another array of of cells which are all set to white background. this array is a copy of the first row in the table
+  // the reason why you use the first row is because you wil; always have at least the first row, and you need it to mimic the amount of columns you will neeed to add to the grid
   const addRow = () => {
-    setGrid((prevGrid) => [...prevGrid, Array(prevGrid[0].length).fill('#ffffff')]);
+    const updatedGrid = [...grid];
+
+    setGrid([...grid, Array(updatedGrid[0].length).fill('#ffffff')]);
+    
   };
 
+  //removing involves slicing out the the last array from the grid of arrays
   const removeRow = () => {
+    const updatedGrid = [...grid];
     if (grid.length > 1) {
-      setGrid((prevGrid) => prevGrid.slice(0, -1));
+      setGrid(updatedGrid.slice(0, -1));
     }
   };
 
+  // by using the .map function, its similar to making a for loop you can access each row value and add the the color value, the index of this value repersents the column 
+  // example [[blue]] --(add a column)--> [[blue,white]]
   const addColumn = () => {
-    setGrid((prevGrid) =>
-      prevGrid.map((row) => {
+    const updatedGrid = [...grid];
+    setGrid( 
+      updatedGrid.map((row) => {
         return [...row, '#ffffff'];
       })
     );
   };
-
+  // you access the the column by using .map on the copy of the current grid, just like how we did in addColumn
+  // you slice the last value within each row array
   const removeColumn = () => {
+    const updatedGrid = [...grid];
     if (grid[0].length > 1) {
-      setGrid((prevGrid) =>
-        prevGrid.map((row) => {
+      setGrid(
+        updatedGrid.map((row) => {
           return row.slice(0, -1);
         })
       );
