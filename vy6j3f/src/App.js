a6b9bb3ed0ae3652d6494aa3f4155b33f61e1971
Abir12918 <banikabir1234@gmail.com>
2023-10-31 14:37:11 -0400

    changed the buttons and styling

--- vy6j3f/src/App.js
+++ vy6j3f/src/App.js
@@ -1,7 +1,6 @@
 import './styles.css';
 import React, { useState } from 'react';
 
-
 function Grid() {
   const [selectedColor, setSelectedColor] = useState('#ffffff');
   const [grid, setGrid] = useState([[selectedColor]]);
@@ -10,18 +9,12 @@ function Grid() {
     setSelectedColor(color);
   };
 
-  // you use a copy of the grid, so it doesnt affect the rendering
-  // when using setGrid you want to return a 2d array: [[],[]]. that is why everything is within the []
-  //...grid will return the array inside: ['white], you are combining this with another array of of cells which are all set to white background. this array is a copy of the first row in the table
-  // the reason why you use the first row is because you wil; always have at least the first row, and you need it to mimic the amount of columns you will neeed to add to the grid
   const addRow = () => {
     const updatedGrid = [...grid];
 
     setGrid([...grid, Array(updatedGrid[0].length).fill('#ffffff')]);
-    
   };
 
-  //removing involves slicing out the the last array from the grid of arrays
   const removeRow = () => {
     const updatedGrid = [...grid];
     if (grid.length > 1) {
@@ -29,18 +22,15 @@ function Grid() {
     }
   };
 
-  // by using the .map function, its similar to making a for loop you can access each row value and add the the color value, the index of this value repersents the column 
-  // example [[blue]] --(add a column)--> [[blue,white]]
   const addColumn = () => {
     const updatedGrid = [...grid];
-    setGrid( 
+    setGrid(
       updatedGrid.map((row) => {
         return [...row, '#ffffff'];
       })
     );
   };
-  // you access the the column by using .map on the copy of the current grid, just like how we did in addColumn
-  // you slice the last value within each row array
+
   const removeColumn = () => {
     const updatedGrid = [...grid];
     if (grid[0].length > 1) {
@@ -53,63 +43,62 @@ function Grid() {
   };
 
   const updateColoredCells = () => {
-   const updatedGrid = [...grid];
-   for(let i = 0; i < updatedGrid.length; i++){
-    for(let j = 0; j < updatedGrid[i].length; j++ ){
-      if(updatedGrid[i][j] === '#ffffff'){
-        updatedGrid[i][j] = '#ffffff';
-      }else{
-        updatedGrid[i][j] = selectedColor;
+    const updatedGrid = [...grid];
+    for (let i = 0; i < updatedGrid.length; i++) {
+      for (let j = 0; j < updatedGrid[i].length; j++) {
+        if (updatedGrid[i][j] !== '#ffffff') {
+          updatedGrid[i][j] = selectedColor;
+        }
       }
     }
-   }
-   setGrid(updatedGrid);
-  }
+    setGrid(updatedGrid);
+  };
 
-  const removeColor = () =>{
+  const removeColor = () => {
     const updatedGrid = [...grid];
-    for(let i = 0; i < updatedGrid.length; i++){
-     for(let j = 0; j < updatedGrid[i].length; j++ ){
-       if(updatedGrid[i][j] != '#ffffff'){
-       updatedGrid[i][j] = '#ffffff';
-       } 
-     }
+    for (let i = 0; i < updatedGrid.length; i++) {
+      for (let j = 0; j < updatedGrid[i].length; j++) {
+        if (updatedGrid[i][j] !== '#ffffff') {
+          updatedGrid[i][j] = '#ffffff';
+        }
+      }
     }
 
     setGrid(updatedGrid);
-   };
+  };
 
   const updateUncoloredCells = () => {
     const updatedGrid = [...grid];
-    for(let i = 0; i < updatedGrid.length; i++){
-     for(let j = 0; j < updatedGrid[i].length; j++ ){
-       if(updatedGrid[i][j] === '#ffffff'){
-        updatedGrid[i][j] = selectedColor
-       }
-     }
+    for (let i = 0; i < updatedGrid.length; i++) {
+      for (let j = 0; j < updatedGrid[i].length; j++) {
+        if (updatedGrid[i][j] === '#ffffff') {
+          updatedGrid[i][j] = selectedColor;
+        }
+      }
     }
     setGrid(updatedGrid);
-   }
-  
+  };
 
   return (
-    <div>
-      <div>
-        Current Color:
+    <div className="container">
+      <div className="color-selector-container">
+        <span className="color-selector-label">SELECT A COLOR HERE:</span>
         <input
           type="color"
+          className="color-selector-input"
           value={selectedColor}
           onChange={(e) => handleColorChange(e.target.value)}
         />
       </div>
-      <button onClick={addRow}>Add Row</button>
-      <button onClick={removeRow}>Remove Row</button>
-      <button onClick={addColumn}>Add Column</button>
-      <button onClick={removeColumn}>Remove Column</button>
-      <button onClick={updateColoredCells}>Update Colored Cells</button>
-      <button onClick={updateUncoloredCells}>Update Uncolored Cells</button>
-      <button onClick={removeColor}>Remove Color</button>
-      
+      <div className="button-container">
+        <button onClick={addRow}>Add Row</button>
+        <button onClick={removeRow}>Remove Row</button>
+        <button onClick={addColumn}>Add Column</button>
+        <button onClick={removeColumn}>Remove Column</button>
+        <button onClick={updateColoredCells}>Update Colored Cells</button>
+        <button onClick={updateUncoloredCells}>Update Uncolored Cells</button>
+        <button onClick={removeColor}>Remove Color</button>
+      </div>
       <table>
         <tbody>
           {grid.map((row, rowIndex) => (
@@ -132,9 +121,6 @@ function Grid() {
     </div>
   );
 }
-
-
-
 
 function Cell({ color, onClick }) {
   return (
